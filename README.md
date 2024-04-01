344485b9b4825ce8604551535b6f942dc526bc63
Saurav Pratihar <pratihar.saurav@gmail.com>
2024-04-01 20:08:24 +0530

    packages updated

--- README.md
+++ README.md
@@ -1,14 +1,5 @@
 This package is a fork of [`react-scripts`](https://npmjs.com/package/react-scripts) that adds a
 couple more features to [Create React App](https://github.com/facebook/create-react-app) tooling:
 
-* Ability to add, build, and test server-side code using the same instance of Webpack.
-* Ability to customize Babel, Webpack and other configs.
-
-
-Please refer to its documentation:
-
-
-- [Getting Started](https://facebook.github.io/create-react-app/docs/getting-started) – How to create a new app.
-- [User Guide](https://facebook.github.io/create-react-app/) – How to develop apps bootstrapped with Create React App.
-- [React App SDK](https://github.com/kriasoft/react-app) - the patched version of CRA
-- [React Starter Kit](https://github.com/kriasoft/react-firebase-starter) - project template (seed project)
+- Ability to add, build, and test server-side code using the same instance of Webpack.
+- Ability to customize Babel, Webpack and other configs.
--- config/webpack.js
+++ config/webpack.js
@@ -1,25 +1,24 @@
 /**
- * Copyright © 2016-present Kriasoft. All rights reserved.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
-'use strict';
+"use strict";
 
-const nodeExternals = require('webpack-node-externals');
-const WriteFilePlugin = require('write-file-webpack-plugin');
-const paths = require('./paths');
+const nodeExternals = require("webpack-node-externals");
+const WriteFilePlugin = require("write-file-webpack-plugin");
+const paths = require("./paths");
 
-module.exports = function(env) {
-  const config = require('./webpack.config')(env, 'node');
+module.exports = function (env) {
+  const config = require("./webpack.config")(env, "node");
   return [
     // Client configuration
-    require('./webpack.config')(env),
+    require("./webpack.config")(env),
     // Server configuration
     Object.assign({}, config, {
-      name: 'server',
-      target: 'node',
+      name: "server",
+      target: "node",
       node: false,
 
       entry: {
@@ -28,8 +27,8 @@ module.exports = function(env) {
 
       output: Object.assign({}, config.output, {
         path: paths.nodeBuild,
-        filename: '[name].js',
-        libraryTarget: 'commonjs2',
+        filename: "[name].js",
+        libraryTarget: "commonjs2",
       }),
 
       optimization: Object.assign({}, config.optimization, {
@@ -44,7 +43,7 @@ module.exports = function(env) {
         }),
       ]),
 
-      externals: ['./stats.json', nodeExternals()],
+      externals: ["./stats.json", nodeExternals()],
     }),
   ];
 };
